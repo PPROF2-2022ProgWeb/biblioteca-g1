d3dae98384964b9834cb6e98abf9e2ac59a59525
Gimena Binaghi <gimenabinaghi@hotmail.com>
2022-09-06 21:25:31 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { DevolucionesComponent } from './components/devoluciones/devoluciones.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LibrosComponent } from './components/libros/libros.component';
+import { PrestamosComponent } from './components/prestamos/prestamos.component';
+import { PrincipalComponent } from './components/principal/principal.component';
+import { ReportesComponent } from './components/reportes/reportes.component';
+import { UsuariosComponent } from './components/usuarios/usuarios.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    DevolucionesComponent,
+    HeaderComponent,
+    LibrosComponent,
+    PrestamosComponent,
+    PrincipalComponent,
+    ReportesComponent,
+    UsuariosComponent
   ],
+
   imports: [
     BrowserModule,
     AppRoutingModule
